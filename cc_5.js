8812ba44e506afffae97a304cc4dbb887d876018
Khe Nguyen <khenguyen@usf.edu>
2025-09-28 23:13:59 -0400

    Created an array

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+// Coding Challenge 5
+
+// Step 2: Employee array
+const employees = [
+  { name: "Havanna Surfside", hourlyRate: 20, hoursWorked: 38 },
+  { name: "Mills Cinnamon", hourlyRate: 22, hoursWorked: 45 },
+  { name: "Pepe Peroni", hourlyRate: 18, hoursWorked: 42 },
+  { name: "Azzurro Maize", hourlyRate: 25, hoursWorked: 40 }
+];
+
